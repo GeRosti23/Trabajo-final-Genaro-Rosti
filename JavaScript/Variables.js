7aa2d26c4b67617de673c5856ef453ac037a9b39
Genaro <genarorosti@hotmail.com>
2022-09-26 14:36:45 -0300

    -git add -git commit -m "CAmbios 26/09" -git push origin master

---
+++ JavaScript/Variables.js
@@ -0,0 +1,8 @@
+var pase = prompt("Que edad tenes?");
+
+if (pase >= 18) {
+    window.alert("Sos mayor, podes continuar");
+}
+else {
+    window.confirm("Estas seguro que desea continuar? La pagina no se hara responsable a partir de este momento.");
+}
